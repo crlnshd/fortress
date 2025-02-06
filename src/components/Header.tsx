cd28dcc29d09f317300139af346fa2270095acd7
Victoria <vika.moiseenko2502@gmail.com>
2025-02-06 14:09:09 +0200

    refactored header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,17 +6,35 @@ type HeaderProps = {
 
 export const Header = ({ headerData }: HeaderProps) => {
   const { logo, nav } = headerData;
+  const handleScroll = (
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+    targetId: string
+  ) => {
+    event.preventDefault();
+
+    const targetElement = document.getElementById(targetId);
+    if (targetElement) {
+      window.scrollTo({
+        top: targetElement.offsetTop,
+      });
+    }
+  };
 
   return (
     <header className="header">
       <img src={logo} alt="Logo" className="logo" />
       <nav>
         <ul className="header-items">
-          {nav.map((item) => (
-            <li key={item.name} className="header-item">
-              <a href={item.url}>{item.name}</a>
-            </li>
-          ))}
+          {nav.map((item) => {
+            const sectionId = item.url.replace("#", "");
+            return (
+              <li key={item.name} className="header-item">
+                <a href={item.url} onClick={(e) => handleScroll(e, sectionId)}>
+                  {item.name}
+                </a>
+              </li>
+            );
+          })}
         </ul>
       </nav>
     </header>
